2e03363c5e8a18ccd660cca600eee60d32ea6037
Mike English <mike.english@gmail.com>
2023-11-07 14:54:58 -0500

    Start building a stack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+trait Nil: Node {}
+
+trait Cons<H, T: Node>: Node {}
+
+trait Node {
+    // fn push<H, C>(self, h: H) -> C
+    // where
+    //     Self: Sized,
+    //     C: Cons<H, Self>;
+}
+
+#[derive(Debug)]
+struct Stack<T: Node> {
+    _phantom: std::marker::PhantomData<T>,
+    data: Vec<u8>,
+}
+
+impl Stack<Empty> {
+    fn new() -> Stack<Empty> {
+        Stack {
+            _phantom: std::marker::PhantomData,
+            data: Vec::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Empty;
+
+impl Node for Empty {}
+impl Nil for Empty {}
+
 fn main() {
-    println!("Hello, world!");
+    let s = Stack::new();
+    dbg!(s);
 }
