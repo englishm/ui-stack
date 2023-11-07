7257fa65ca04f0c5aafb94bcc8e892b66d0df16d
Mike English <mike.english@gmail.com>
2023-11-07 15:23:01 -0500

    Put stuff on the stack

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,7 @@ trait Nil: Node {}
 
 trait Cons<H, T: Node>: Node {}
 
-trait Node {
-    // fn push<H, C>(self, h: H) -> C
-    // where
-    //     Self: Sized,
-    //     C: Cons<H, Self>;
-}
+trait Node {}
 
 #[derive(Debug)]
 struct Stack<T: Node> {
@@ -24,13 +19,50 @@ impl Stack<Empty> {
     }
 }
 
+impl<T: Node> Stack<T> {
+    fn push<H>(self: Stack<T>, h: H) -> Stack<NonEmpty<H, T>>
+    where
+        Self: Sized,
+        H: Sized,
+    {
+        let mut data = self.data;
+        let h_as_bytes = unsafe {
+            std::slice::from_raw_parts(&h as *const H as *const u8, std::mem::size_of::<H>())
+        };
+        data.extend(h_as_bytes);
+        Stack {
+            _phantom: std::marker::PhantomData,
+            data: data,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Empty;
 
 impl Node for Empty {}
 impl Nil for Empty {}
 
+#[derive(Debug)]
+struct NonEmpty<H, T: Node> {
+    _phantom: std::marker::PhantomData<(H, T)>,
+}
+
+impl<H, T> Node for NonEmpty<H, T>
+where
+    H: Sized,
+    T: Node,
+{
+}
+impl<H, T> Cons<H, T> for NonEmpty<H, T>
+where
+    H: Sized,
+    T: Node,
+{
+}
+
 fn main() {
     let s = Stack::new();
+    let s = s.push(1u8);
     dbg!(s);
 }
