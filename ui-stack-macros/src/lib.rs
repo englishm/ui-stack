ed8acefdbe8097c46211831c62341334a74a83ed
Mike English <mike.english@gmail.com>
2023-11-14 17:00:31 -0500

    WIP

--- ui-stack-macros/src/lib.rs
+++ ui-stack-macros/src/lib.rs
@@ -1,6 +1,11 @@
 use proc_macro::TokenStream;
 
 #[proc_macro]
-pub fn pop_statement(item: TokenStream) -> TokenStream {
-    item
+pub fn pop_statement(token_stream: TokenStream) -> TokenStream {
+    let mut tokens = token_stream.into_iter();
+    let mut output = String::new();
+    while let Some(token) = tokens.next() {
+        output.push_str(&token.to_string());
+    }
+    output.parse().unwrap()
 }
